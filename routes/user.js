ac214d7084c5227746184cdf11c1516fdb0320ae
Mar550 <marouane.merno@gmail.com>
2022-07-30 17:15:11 +0100

    FIND route

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,10 @@
 const User = require("../models/User");
-const {verifyToken, verifyTokenAndAut} = require("./verifyToken");
+const {verifyToken, verifyTokenAndAut, verifyTokenAndAdmin} = require("./verifyToken");
 const router = require("express").Router();
 const CryptoJS = require("crypto-js");
 
 
-// Update route
+// UPDATE route
 
 router.put("/:id", verifyTokenAndAut, async (req, res) => {
     const checkedid = req.params.id;
@@ -31,5 +31,15 @@ router.put("/:id", verifyTokenAndAut, async (req, res) => {
   });
 
 
-
+// FIND route
+router.get("/find/:id", verifyTokenAndAdmin, async (req,res)=>{
+  const checkedid = req.params.id;
+  try{
+    const user = await User.findById(checkedid.trim())
+    const { password, ...others } = user._doc;
+    res.status(200).json(others);
+  } catch(err){
+    res.status(500).json(err);
+  }
+})
 module.exports = router;
