f6d89d80ff187af6957e134ad76a44e619baa990
Mar550 <marouane.merno@gmail.com>
2022-08-07 19:48:40 +0100

    Products filters and sort api

--- src/components/Products.js
+++ src/components/Products.js
@@ -7,6 +7,7 @@ import Product from './Product';
 
 const Products = ({cat, filters, sort}) => {
   const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
 
   // Add header allow axios
   useEffect(() => {
@@ -22,14 +23,40 @@ const Products = ({cat, filters, sort}) => {
     };
     getProducts();
   }, [cat]);
- 
+  
+  useEffect(() => {
+    cat &&
+      setFilteredProducts(
+        products.filter((item) => 
+          Object.entries(filters).every(([key, value]) =>
+               item[key].includes(value)
+            )
+          )
+        );
+  }, [products, cat, filters]);
+
+  useEffect(() => {
+    if (sort === "newest") {
+      setFilteredProducts((prev) =>
+        [...prev].sort((a, b) => a.createdAt - b.createdAt)
+      );
+    } else if (sort === "asc") {
+      setFilteredProducts((prev) =>
+        [...prev].sort((a, b) => a.price - b.price)
+      );
+    } else {
+      setFilteredProducts((prev) =>
+        [...prev].sort((a, b) => b.price - a.price)
+      );
+    }
+  }, [sort]);
+
   return (
     <Wrapper>
         <h1 className="title"> Our popular products </h1>
         <div className="container">
-        {products.map((item) => (
-            <Product item={item} key={item.id} />
-        ))};
+          {cat ? filteredProducts.map((item) => <Product item={item} key={item.id} />)
+          : products.slice(0, 8).map((item) => <Product item={item} key={item.id} />)}
         </div>
     </Wrapper>
   )
